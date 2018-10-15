54a843e748842a1dfaed273aa9a43fc6962bceb0
lxlbz3696096 <38748161+lxlbz3696096@users.noreply.github.com>
2018-10-15 14:14:46 +0800

    微服务监控

---
+++ lx-monitor/src/main/java/com/prometheus/application/MonitorApplication.java
@@ -0,0 +1,18 @@
+package com.prometheus.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class MonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorApplication.class, args);
+	}
+
+}
